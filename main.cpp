fe79ec2b044686cd446eba929cbb179fce51e922
Chatrapati Ākula <chatrapathi.akula@gmail.com>
2022-03-11 10:50:28 +0000

    various copy methods

--- main.cpp
+++ main.cpp
@@ -1,62 +1,40 @@
-#include <algorithm>          // std::remove
-#include <cctype>             // std::isspace
-#include <iostream>           // std::cin
-#include <string>             // std::string
-#include <string_view>        // std::string_view
+#include <algorithm>
+#include <iostream>
+#include <string>
 
 int main()
 {
-        // Copy, Substring, Find, Join
-        std::string name {};
-        std::cout << "Enter your full name (Firstname Lastname): ";
-        std::getline(std::cin, name);
+        std::string fullname {};
 
-        // We're assuming that we're using the UK naming convention. Firstname Lastname
-        std::string firstname {};
-        std::string lastname {};
+        std::cout << "Please enter your fullname (Firstname Lastname): ";
+        std::getline(std::cin, fullname);        // get name from user
 
-        // "Amine Mihoubi"
-        // Task: Check if the input string contains no space characters in the front.
+        // Copy #1
+        // std::string fullname_copy {};
+        // fullname_copy = fullname;
 
-        constexpr auto is_space = [](const char c) { return c == ' '; };
-        const auto  sp = std::find_if_not(name.begin(), name.end(), is_space);
-        if (sp != name.end())
-        {
-                std::cout << sp[0] << '\n';
+        // Copy #2
+        // std::string fullname_copy {fullname};
 
-                const auto beg       = name.find(*sp);
-                // print the first and lastnames as Firstname.Lastname
-                const auto space_pos = name.find(' ', beg);
-                std::cout << space_pos - beg << '\n';
-                firstname = name.substr(beg, space_pos - beg);        // get the first part of the string until ' '
-                lastname  = name.substr(space_pos + 1);               // get the rest of the string
+        // Copy #3
+        // std::string fullname_copy(fullname.size(), '\0');
+        // fullname.copy(fullname_copy.data(), fullname_copy.size());
 
-                // put the first and lastnames together joined by a '.'
-                std::string fullname {firstname};
-                (fullname += '.') += lastname;
-                std::cout << fullname << '\n';
-        }
+        // Copy #4
+        // std::string fullname_copy(fullname.size(), '\0');
+        // std::copy(fullname.begin(), fullname.end(), fullname_copy.begin());
+        // std::cout << "fullname: " << fullname << '\n';
+        // std::cout << "fullname_copy: " << fullname_copy << '\n';
 
-        // lstrip or trim or remove the leading space character
-        // while (true)
-        // {
-        //         if (name[0] != ' ') { break; }
+        // Copy #5
+        std::string fullname_copy {};
+        fullname_copy += fullname;
+        std::cout << "fullname: " << fullname << '\n';
+        std::cout << "fullname_copy: " << fullname_copy << '\n';
 
-        //         // find the location of the first space character starting from the beginning to the end of name
-        //         const auto space_pos = std::find(name.begin(), name.end(), ' ');
-        //         // remove the space character at this location
-        //         const auto c         = name.erase(space_pos);
-        //         if (c[0] != ' ') { break; }
-        // }
-        // std::cout << name << '\n';
+        // Task 1: Copy the date from the user input into another variable.
+        // Task 2: Copy the date from the user input into the end of another variable. (Hint: The destination must already contain a string).
 
-        // // print the first and lastnames as Firstname.Lastname
-        // const auto space_pos = name.find(' ');
-        // firstname            = name.substr(0, space_pos);         // get the first part of the string until ' '
-        // lastname             = name.substr(space_pos + 1);        // get the rest of the string
-
-        // // put the first and lastnames together joined by a '.'
-        // std::string fullname {firstname};
-        // (fullname += '.') += lastname;
-        // std::cout << fullname << '\n';
+        // Example - Count the vowels in a given string.
+        
 }
