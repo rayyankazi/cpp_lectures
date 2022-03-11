d383d4e730476978fdac0f44c0e36805f0f50587
Chatrapati Ākula <chatrapathi.akula@gmail.com>
2022-03-11 11:25:07 +0000

    count all vowels in string

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
+#include <string_view>
 
 int main()
 {
-        std::string fullname {};
+        // std::string fullname {};
 
-        std::cout << "Please enter your fullname (Firstname Lastname): ";
-        std::getline(std::cin, fullname);        // get name from user
+        // std::cout << "Please enter your fullname (Firstname Lastname): ";
+        // std::getline(std::cin, fullname);        // get name from user
 
         // Copy #1
         // std::string fullname_copy {};
@@ -27,14 +28,55 @@ int main()
         // std::cout << "fullname_copy: " << fullname_copy << '\n';
 
         // Copy #5
-        std::string fullname_copy {};
-        fullname_copy += fullname;
-        std::cout << "fullname: " << fullname << '\n';
-        std::cout << "fullname_copy: " << fullname_copy << '\n';
+        // std::string fullname_copy {};
+        // fullname_copy += fullname;
+        // std::cout << "fullname: " << fullname << '\n';
+        // std::cout << "fullname_copy: " << fullname_copy << '\n';
 
         // Task 1: Copy the date from the user input into another variable.
         // Task 2: Copy the date from the user input into the end of another variable. (Hint: The destination must already contain a string).
 
         // Example - Count the vowels in a given string.
-        
+        // aeiou
+        // The Quick Brown Fox Jumps Over the Lazy Dog
+
+        std::string_view str {"The Quick Brown Fox Jumps Over the Lazy Dog"};
+        // int              num_vowels {0};
+        // Method #1
+        // for (const auto c : str)
+        // {
+        //         const auto ch = std::tolower(c);
+        //         if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') { num_vowels += 1; }
+        // }
+
+        // Method #2
+        // for (const auto v : "AaEeIiOoUu")
+        // {
+        //         // take vowel and find all occurences of it in str.
+        //         int i = 0;
+        //         while (true)
+        //         {
+        //                 i = str.find(v, i);
+        //                 if (i != std::string_view::npos)
+        //                 {
+        //                         num_vowels += 1;
+        //                         i += 1;
+        //                 }
+        //                 else { break; }
+        //         }
+        // }
+
+        // Method #3
+        // std::string_view vowels {"AaEeIiOoUu"};
+        // for (int i = 0;;)
+        // {
+        //         i = str.find_first_of(vowels, i);
+        //         if (i != std::string_view::npos)
+        //         {
+        //                 num_vowels += 1;
+        //                 i += 1;
+        //         }
+        //         else { break; }
+        // }
+        // std::cout << num_vowels << '\n';
 }
