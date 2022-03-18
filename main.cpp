cd03c5cfaf14094d82e7912d9cfb1b21e0c565f2
Chatrapati Ākula <chatrapathi.akula@gmail.com>
2022-03-18 12:07:27 +0000

    pointers and pointer arithmetic

--- main.cpp
+++ main.cpp
@@ -1,99 +1,81 @@
-#include <algorithm>        // std::accumulate, std::reduce
-#include <array>            // std::array
-#include <iostream>         // std::cout, std::cin, std::scanf
-#include <iterator>         // std::istream_iterator
-#include <sstream>
-#include <string>         // std::string
-#include <utility>        // std::pair
+#include <algorithm>
+#include <array>
+#include <cstdint>
+#include <iostream>
+#include <string>
 
-constexpr auto NUM_FUNCTIONS = 7;
-
-auto           ask_user_continue() -> bool
+auto main() -> int
 {
-        std::cout << "Would you like to continue (y/n): ";
-        char opt {};
-        std::cin >> opt;
-        return opt != 'n';
-}
+        // RAM - Random Access Memory (Volatile Storage, Memory)
+        // [||||||...|||] -> 1 byte per cell
+        // 1 byte = 8 bits (binary digit)
+        // 1024 bytes = 1KB
+        // 1024 KB = 1MB
+        // 1024 MB = 1GB
+        // 1024 GB = 1TB
+        int     x {4};           // 4 bytes = 32 bits
+        int16_t x16 {};          // 2 bytes = 16 bits
+        auto    p {&x16};        // p is a variable that holds the address or location of x16
+        // p is therefore called a pointer (variable) to x16
 
-auto ask_user_arithemtic() -> int
-{
-        std::cout << "Press 'a' for addition." << '\n';
-        std::cout << "Press 's' for subtraction." << '\n';
-        std::cout << "Press 'm' for multiplication." << '\n';
-        std::cout << "Press 'd' for division." << '\n';
-        std::cout << "Press 'r' for remainder." << '\n';
-        std::cout << "Press 'p' for power." << '\n';
-
-        char opt {};
-        std::cin >> opt;
-        switch (opt)
         {
-                case 'a': return 1;
-                case 's': return 2;
-                case 'm': return 3;
-                case 'd': return 4;
-                case 'r': return 5;
-                case 'p': return 6;
-                default: return 0;
+                auto x {46};
+                auto px {&x};        // address of an integer
+
+                auto y {3.f};
+                auto py {&y};
+                auto yy {4.13f};
+                py = &yy;
+
+                auto               z {"Google"};
+                auto               pz {&z};
+
+                // Array to Pointer Decay
+                // An array's name is just a pointer to the beginning of the array.
+                std::array<int, 3> ax {1, 2, 3};
+                std::printf("%p -> %d\n", ax.begin() + 0, *(ax.begin() + 0));
+                std::printf("%p -> %d\n", ax.begin() + 1, *(ax.begin() + 1));
+                std::printf("%p -> %d\n", ax.begin() + 2, *(ax.begin() + 2));
+
+                // NOTE!!!!!: You cannot take the address of a literal.
+                // Example: auto px = &4; // INCORRECT
+                // Example: auto px = &4.45f; // INCORRECT
+                // Example: auto px = &"hello world"; // INCORRECT
+
+                constexpr int      N = 4;
+
+                // NOTE!!!! end() will give the address of the immediate next address from the last item in array/vector
+                std::array<int, N> a {1, 2, 3, 4};
+                // auto               p   = a.begin();          // address of first item in array
+                // std::printf("%p\n", p); // print the address of item pointed to by p
+                // p += 1;
+                // std::printf("%p\n", p); // print the address of item pointed to by p
+                // p = std::next(p);
+                // std::printf("%p\n", p); // print the address of item pointed to by p
+
+                auto               q = a.end() - 1;        // address of last item in array
+                std::printf("%p -> %d\n", q, *q);
+                q -= 1;
+                std::printf("%p -> %d\n", q, *q);
+
+                // auto               tmp = -1;
+
+                // Dereferencing -> Get the item/value at address
+                // Referencing -> Get the address of item/variable
+                // Pointer Arithmetic
+
+                // while (p < q)
+                // {
+                //         tmp = *p; // Dereference p and copy item into tmp, p = &a[0]
+                //         *p = *q; // *q -> gets the item from the address in q, *p <- *q => a = {4, 2, 3, 4}
+                //         p += 1; // move the p forward -> pointer arithmetic
+
+                //         *q = tmp; // a = {4, 2, 3, 1}
+                //         q -= 1; // move the q backward
+                // }
+
+                // std::reverse(a.begin(), a.end());
+                std::for_each(a.begin(), a.end(), [](const auto v) { std::cout << v << '\n'; });
+                // a => {4, 3, 2, 1} -> Reversing the array a
         }
-}
-
-auto ask_user_numbers() -> std::pair<float, float>
-{
-        std::cout << "Enter two numbers (x y): ";
-        float x {}, y {};
-        std::scanf("%f %f", &x, &y);
-        return std::make_pair(x, y);
-}
-
-// Task 1 -> Implement the power and remainder operations.
-// Task 2 -> Declare the `res` variable.
-// Task 3 -> Create an array containing functions that perform arithmetic. (Hint: See example)
-
-// Keyword: Function Signature -> ReturnType (Parameters...)
-using BinaryFn = float (*)(float, float);
-
-constexpr auto                                nop(float x, float y) -> float { return 0; }
-
-constexpr auto                                add(float x, float y) -> float { return x + y; }
-
-constexpr std::array<BinaryFn, NUM_FUNCTIONS> FN {nullptr, add};
-
-auto                                          main() -> int
-{
-        /*
-        std::cout << "Calculator v0.3" << '\n';
-
-        char opt {};
-        do {
-                const auto op     = ask_user_arithemtic();
-                const auto [a, b] = ask_user_numbers();
-                if (op < 1 || op > 6)
-                {
-                        std::cout << "Unknown operation selected. Please select again." << '\n';
-                        continue;
-                }
-                const auto fn = FN[op];
-                std::cout << "Result: " << fn(a, b) << '\n';
-                if (!ask_user_continue()) { break; }
-        } while (true);
-        */
-
-        // Example: User gives a list of numbers to compute the sum of.
-        std::cout << "Enter a list of numbers separated by a space (0 to terminate): ";
-        std::vector<float> nums {};
-        float              num {};
-        do {
-                std::cin >> num;
-                if (num == 0) { break; }
-                nums.push_back(num);
-        } while (true);
-
-        // std::istream_iterator to process directly from std::cin
-        // Pointers (*), References (&), nullptr, Lambda Expressions
-        // Structs
-        float sum {};
-        std::for_each(nums.begin(), nums.end(), [&](float x) { sum += x; });
-        std::cout << sum << '\n';
 }
