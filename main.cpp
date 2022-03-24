51cf7e1d7addd5c4359b20e8393956e2c9c403e1
Chatrapati Ākula <chatrapathi.akula@gmail.com>
2022-03-24 16:33:50 +0000

    extract_words using algorithms

--- main.cpp
+++ main.cpp
@@ -1,32 +1,35 @@
 #include <algorithm>
 #include <iostream>
+#include <string>
 #include <vector>
 
-// Find, Modify, Filter
+// The Sky is blue -> [The, Sky, is, blue]
+// TheSkyBlue
 
-auto get_numbers()
+using Words = std::vector<std::string>;
+
+auto extract_words(Words& words, std::string text, std::string_view separators)
 {
-        std::vector<int> nums {};
-        std::cout << "Enter a list of numbers (0 to terminate): ";
-        do {
-                int num {};
-                std::cin >> num;
-                nums.push_back(num);
-                if (num == 0) { break; }
-        } while (true);
+        auto   start = text.find_first_not_of(separators);
+        size_t end {};
 
-        return nums;
+        while (start != std::string::npos)
+        {
+                end = text.find_first_of(separators, start + 1);
+                if (end == std::string_view::npos) { end = text.length(); }
+                std::string s {text.substr(start, end - start)};
+                words.push_back(s);
+                start = text.find_first_not_of(separators, end + 1);
+        }
 }
-
-auto is_odd(int x) { return (x % 2 != 0); }
 
 auto main() -> int
 {
-        auto nums = get_numbers();
+        std::cout << "Enter some text: ";
+        std::string input {};
+        std::getline(std::cin, input);
+        Words words {};
+        extract_words(words, input, " \t");
 
-        // find the first odd number
-        // auto podd = std::find_if(nums.begin(), nums.end(), is_odd);
-        // std::cout << *podd << '\n';
-
-        // count_if -> count the no. of even numbers
+        std::for_each(words.begin(), words.end(), [](auto& w) { std::cout << w << '\n'; });
 }
