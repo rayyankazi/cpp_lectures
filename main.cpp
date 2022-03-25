cf7c958d3a93b658be0b274bbe08a0141e2cc03d
Chatrapati Ākula <chatrapathi.akula@gmail.com>
2022-03-25 10:30:25 +0000

    enumeration colors

--- main.cpp
+++ main.cpp
@@ -1,44 +1,31 @@
-#include <algorithm>
+// T2 - Beginning C++17
+// Chapter 3, Pg. 77 - 80
+// Chapter 11, Pg. 386
+
+// Enumerations and Structures
+
+#include <array>
 #include <iostream>
-#include <vector>
 
-enum class ShoeBrands
+// Colors
+enum class Color
 {
-        Nike,
-        Adidas,
-        Clarks,
+        Red,
+        Green,
+        Blue,
+        Count
 };
 
-struct DatabaseItem
+// Properties
+enum class ColorProperty
 {
-        ShoeBrands brand;
-        float      cost;
+        Saturation,
+        Brightness,
+        Value,
 };
 
-auto operator==(DatabaseItem& lhs, DatabaseItem& rhs) -> bool { return lhs.brand == rhs.brand; }
+constexpr std::array<std::string_view, static_cast<int>(Color::Count)> COLORS {"Red", "Green", "Blue"};
 
-auto main() -> int
-{
-        DatabaseItem x1 {ShoeBrands::Adidas, 45.F};
-        DatabaseItem x2 {ShoeBrands::Nike, 70.F};
-        const auto   is_same = x1 == x2;
-        std::cout << is_same << '\n';
+auto                                                      get_color(Color c) { return COLORS[static_cast<int>(c)]; }
 
-        /*
-        std::vector<DatabaseItem> database {};
-
-        std::cin >> brand_input;
-
-        std::vector<int> numbers {};
-        while (true)
-        {
-                int number {};
-                std::cin >> number;
-                numbers.push_back(number);
-                if (number == 0) { break; }
-        }
-
-        const auto n = std::count_if(numbers.begin(), numbers.end(), [](int x) { return (x % 2 != 0); });
-        std::cout << "Number of odd numbers: " << n << '\n';
-        */
-}
+auto                                                      main() -> int { std::cout << get_color(Color::Green) << '\n'; }
