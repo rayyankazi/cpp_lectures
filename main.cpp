9b90f1f05e109d564ddabe200653b9df838e5f3c
Chatrapati Ākula <chatrapathi.akula@gmail.com>
2022-03-28 09:30:32 +0000

    Item, Items and Search

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
 
 enum class Products
 {
@@ -20,18 +22,97 @@ enum class Products
         productsEnd,
 };
 
-auto is_product(Products p)
+std::vector<std::string> PRODUCT_NAMES {
+        "",
+        "",
+        "Galaxy",
+        "Note",
+        "ZFold",
+        "",
+        "",
+        "iPhone",
+};
+
+
+struct Item
 {
-        return p > Products::productsStart && p < Products::productsEnd;
+        Products    product;
+        int         stock;
+        float       price;
+        std::string name;
+
+        Item() = default;
+        Item(Products p, int num_stock, float price, std::string_view name) : product {p}, stock {num_stock}, price {price}, name {name} {}
+};
+
+auto is_product(Products p) { return p > Products::productsStart && p < Products::productsEnd; }
+
+auto is_apple_product(Products p) { return p > Products::appleStart && p < Products::appleStop; }
+
+auto get_product_details()
+{
+        Item item {};
+
+        int  product_id {};
+
+        std::cout << "Select product: ";
+        std::cin >> product_id;
+        if (is_product(static_cast<Products>(product_id)))
+        {
+                std::cout << "Enter stock: ";
+                std::cin >> item.stock;
+                std::cout << "Enter price: ";
+                std::cin >> item.price;
+                std::cout << "Enter name: ";
+                std::cin >> item.name;
+        }
+
+        return item;
 }
 
-auto is_apple_product(Products p)
+using Items = std::vector<Item>;
+
+auto search_item(const Items& items)
 {
-        return p > Products::appleStart && p < Products::appleStop;
+        char sopt {};
+        std::cout << "(n)Search by Name, (p)Search by Product: ";
+        std::cin >> sopt;
+        if (sopt == 'n')
+        {
+                std::string name {};
+                std::cout << "Enter product name: ";
+                std::cin >> name;
+                return std::find_if(items.begin(), items.end(), [&](const Item& item) { return item.name == name; });
+        }
+
+        return items.end();        // if nothing is found then return the end of the items
 }
 
 auto main() -> int
 {
-        int opt {7};
-        std::cout << is_apple_product(static_cast<Products>(opt)) << '\n';
+        Items items {};
+
+        char opt {};
+        do {
+                std::cout << "(a)Add, (r)Remove, (e)Edit, (s)Search: ";
+                std::scanf(" %c", &opt);
+
+                if (opt == 'a')
+                {
+                        // adding item
+                        const auto item = get_product_details();
+                        items.push_back(item);
+                }
+                else if (opt == 's')
+                {
+                        const auto pitem = search_item(items);
+                        std::cout << pitem->name << '\n';
+                }
+                else
+                {
+                        // invalid user option
+                        std::cout << "Invalid option selected. Please try again." << '\n';
+                        break;
+                }
+        } while (true);
 }
