987184b4d4c7c8b56c44b5b84200dd7e690138dd
Chatrapati Ākula <chatrapathi.akula@gmail.com>
2022-03-28 13:48:29 +0000

    complete example demonstrating enum class + struct

--- main.cpp
+++ main.cpp
@@ -1,24 +1,222 @@
 #include <algorithm>
+#include <cmath>        // pow, fmod
+#include <functional>
 #include <iostream>
 #include <vector>
+#include <optional>
+
+/// Calc is an extensible calculator that in addition to performing the basic operations
+/// enables the user to add and perform new, but predefined, operations.
+
+// List of arithmetic operations available in the calculator.
+enum class Operations
+{
+        Invalid = -1,
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division,
+        Power,
+        Remainder,
+        Count
+};
+
+// OP_NAMES holds the names of the operations available.
+constexpr std::string_view OP_NAMES[static_cast<int>(Operations::Count)] = {
+    [static_cast<int>(Operations::Addition)]       = "Addition",
+    [static_cast<int>(Operations::Subtraction)]    = "Subtraction",
+    [static_cast<int>(Operations::Multiplication)] = "Multiplication",
+    [static_cast<int>(Operations::Division)]       = "Division",
+    [static_cast<int>(Operations::Power)]          = "Power",
+    [static_cast<int>(Operations::Remainder)]      = "Remainder",
+};
+
+auto is_valid_op(Operations op) { return op > Operations::Invalid && op < Operations::Count; }
+
+auto is_default_op(Operations op) -> bool { return (op != Operations::Power) && (op != Operations::Remainder); }
+
+auto get_op_name(Operations op)
+{
+        if (!is_valid_op(op)) { return std::string_view {""}; }
+
+        return OP_NAMES[static_cast<int>(op)];
+}
+
+// An Instruction represents the arithmetic operation that a calculator can perform.
+// Every instruction has the following structure <Operation Function>.
+struct Instruction
+{
+        using binary_fn = std::function<float(float, float)>;
+
+        Operations op;
+        binary_fn  fn;
+
+        Instruction() : op {Operations::Invalid}, fn {nullptr} {}
+        Instruction(Operations op, binary_fn fn) : op {op}, fn {fn} {}
+};
+
+// An extensible calculator that in addition to performing the basic operations
+// enables the user to add and perform new, but predefined, operations.
+struct Calculator
+{
+        using Ops = std::vector<Instruction>;
+
+        enum class Error
+        {
+                Ok,
+                InvalidOperationSelected,
+                OperationAlreadyExists,
+        };
+
+        Ops ops;
+
+        Calculator()
+        {
+                ops.reserve(static_cast<int>(Operations::Count));        // reserve space for the operations we have
+
+                // add our default operations
+                ops.emplace_back(Operations::Addition, [](float lhs, float rhs) { return lhs + rhs; });
+                ops.emplace_back(Operations::Subtraction, [](float lhs, float rhs) { return lhs - rhs; });
+                ops.emplace_back(Operations::Multiplication, [](float lhs, float rhs) { return lhs * rhs; });
+                ops.emplace_back(Operations::Division, [](float lhs, float rhs) { return lhs / rhs; });
+        }
+
+        auto compute(Operations op, float lhs, float rhs) -> std::optional<float>
+        {
+                const auto pinstr = std::find_if(ops.begin(), ops.end(), [op](const auto& i) { return i.op == op; });
+                if (pinstr != ops.end()) { return {pinstr->fn(lhs, rhs)}; }
+
+                return {};
+        }
+
+        auto add_new_op(Operations op)
+        {
+                // check if valid operation
+                if (!is_valid_op(op)) { return Error::InvalidOperationSelected; }
+
+                // check if op is default op
+                if (!is_default_op(op))
+                {
+                        // check if already exists in our store
+                        if (std::none_of(ops.begin(), ops.end(), [&](const auto& i) { return i.op == op; }))
+                        {
+                                // add the requested operation
+                                if (op == Operations::Power)
+                                {
+                                        ops.emplace_back(op, [](float lhs, float rhs) { return std::pow(lhs, rhs); });
+                                }
+                                else if (op == Operations::Remainder)
+                                {
+                                        ops.emplace_back(op, [](float lhs, float rhs) { return std::fmod(lhs, rhs); });
+                                }
+
+                                return Error::Ok;
+                        }
+                }
+
+                return Error::OperationAlreadyExists;
+        }
+
+        // Display the list of operations available to the user.
+        auto print_ops()
+        {
+                std::cout << "List of operations: " << '\n';
+                for (auto& i : ops)
+                {
+                        auto name = get_op_name(i.op).data();
+                        std::printf("(%d) %s\n", static_cast<int>(i.op), name);
+                }
+        }
+};
+
+constexpr std::string_view InvalidOperationSelectedMessage = "Invalid operation selected. Please try again.\n";
+
+struct CalculatorUI
+{
+        enum class Signal
+        {
+                Ok,
+                InvalidOption,
+                Quit,
+        };
+
+        Calculator calc;
+
+        CalculatorUI() : calc {} {}
+
+        auto user_option_handler() -> std::function<Signal()>
+        {
+                char opt {};
+                std::cout << "Select function (c)Compute (n)New Operation (q)Quit: ";
+                std::cin >> opt;
+
+                if (opt == 'n')
+                {
+                        return [this]() {
+                                int opt {};
+                                std::cout << "(4)Power, (5)Remainder: ";
+                                std::cin >> opt;
+
+                                const auto err = calc.add_new_op(static_cast<Operations>(opt));
+                                if (err != Calculator::Error::Ok && err != Calculator::Error::InvalidOperationSelected)
+                                {
+                                        std::cout << InvalidOperationSelectedMessage;
+                                        return Signal::InvalidOption;
+                                }
+
+                                return Signal::Ok;
+                        };
+                }
+
+                if (opt == 'c')
+                {
+                        return [this]() {
+                                calc.print_ops();
+
+                                int op {};
+                                std::cin >> op;
+
+                                const auto uop = static_cast<Operations>(op);        // user selected operation
+                                if (!is_valid_op(uop))
+                                {
+                                        std::cout << InvalidOperationSelectedMessage;
+                                        return Signal::InvalidOption;
+                                }
+
+                                // get numbers from user
+                                float lhs {}, rhs {};
+                                std::cout << "Enter arguments (a b): ";
+                                std::cin >> lhs;
+                                std::cin >> rhs;
+
+                                // compute result and display to user
+                                const auto res = calc.compute(uop, lhs, rhs);
+                                if (res) { std::cout << "Result = " << res.value() << '\n'; }
+
+                                return Signal::Ok;
+                        };
+                }
+
+                if (opt == 'q')
+                {
+                        return [this]() { return Signal::Quit; };
+                }
+
+                return [this]() { return Signal::InvalidOption; };
+        }
+
+        auto run()
+        {
+                std::cout << "Calc v0.1.\n";
+                do {
+                        const auto handler = user_option_handler();
+                        if (handler() == CalculatorUI::Signal::Quit) { break; }
+                } while (true);
+        }
+};
 
 auto main() -> int
 {
-        std::vector<int> nums {};
-
-        std::cout << "Enter numbers (0 to terminate): ";
-        do {
-                int num {};
-                std::cin >> num;
-                nums.push_back(num);
-                if (num == 0) { break; }
-        } while (true);
-
-        // find the first odd number and delete it
-        int unum {8};
-        std::cin >> unum;
-
-        nums.erase(std::find_if(nums.begin(), nums.end(), [&](const auto& n) { return n == unum; }));
-        // print items in nums
-        std::for_each(nums.begin(), nums.end(), [](const auto& n) { std::cout << n << '\n'; });
+        CalculatorUI calc_ui {};
+        calc_ui.run();
 }
